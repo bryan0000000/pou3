9a8bf38683a1027ccfcd9915ad83641007f5b68e
bryuan <kauan220656@gmail.com>
2024-04-09 19:10:12 -0300

    fyhjgffju

--- pou3/MainPage.xaml.cs
+++ pou3/MainPage.xaml.cs
@@ -13,19 +13,57 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+
+      
         
         susto = new boou();
         poodol = new creatina();
         pinscher = new pinscher01();
         atual = new dog1();
+
+        atual = poodol;
         
-        Progresscomida.Progress = atual.comida;
-        Progressagua.Progress= atual.agua;
-        Progressdiversao.Progress= atual.diversao;
+       Progresscomida.Progress = atual.comida;
+       Progressagua.Progress= atual.agua;
+       Progressdiversao.Progress= atual.diversao;
+        
 
-
-        imgpersonage.Source = atual.GetnomedaFoto();
     }
 
+    void botaotrocar(object sender, EventArgs args)
+	{
+	   if(atual == poodol)
+	   { 
+		  atual = susto;
+	   }
+	   else if(atual == susto)
+	   { 
+		 atual = pinscher;
+	   }
+	   else if (atual == pinscher)
+	   {
+		 atual = poodol;
+	   }
+    
+       imgpersonage.Source = atual.GetnomedaFoto();
+    }
+
+   void botaofome(object sender, EventArgs args)
+   	{
+      	atual.setcomida(atual.setcomida() + 0.1);
+		
+   	}
+
+      void botaoagua(object sender, EventArgs args)
+   	{
+      	atual.setagua(atual.setagua() + 0.1);
+	
+   	}
+      void botaodivercao(object sender, EventArgs args)
+   	{
+      	atual.setdiversao(atual.setdiversao() + 0.1);
+	
+   	}
+     
 }
 
--- pou3/creatina.cs
+++ pou3/creatina.cs
@@ -5,6 +5,7 @@ public class creatina : dog1
     public creatina()
     {
       nomedaFoto="creatina.jpg";
+      
       comida = 0.1;
       agua = 0.1;
       diversao = 0.9;
